fcbab66b3e4a700c123976b358f47051eeb26d2a
masamiles <36736874+masamiles@users.noreply.github.com>
2019-08-11 12:31:31 -0300

    Tela de login

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 Painel de gerenciamento do aplicativo Partiu feito em Angular 8.
 Necessita NodeJs e Angular/cli versão 8
 NodeJs: https://nodejs.org/en/download/
-<<<<<<< HEAD
-npm install -g @angular/cli
-=======
-npm install -g @angular/cli
->>>>>>> f29ceb91e3d8dea059821ddb227dbf9a0ee53021
+Angular: npm install -g @angular/cli
+Bootstrap: npm install ngx-bootstrap --save
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'Login', component: LoginComponent },
+  { path: '', redirectTo: '/Login', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
