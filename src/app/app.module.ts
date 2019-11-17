0e7225c4d0eab1f956f84905e39981e5feeafa36
masamiles <36736874+masamiles@users.noreply.github.com>
2019-11-17 15:21:02 -0300

    refatoração de layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,8 @@ import { RelatorioComponent } from './relatorio/relatorio.component';
 import { MineracaoComponent } from './mineracao/mineracao.component';
 import { NgxCurrencyModule } from "ngx-currency";
 import { RatingModule } from 'ng-starrating';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Material from '@angular/material';
 
 
 const appRoutes: Routes = [
@@ -67,6 +69,7 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
       { enableTracing: true } // <-- debugging purposes only
     ),
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
@@ -74,7 +77,8 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
     ChartsModule,
     NgxMaskModule.forRoot(options),
     NgxCurrencyModule,
-    RatingModule
+    RatingModule,
+    Material.MatSidenavModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent], 
